780474df862f54ecc9ca8ec85243a8e56f0a9c07
daniellaw <daniellaw04@yahoo.com.hk>
2024-04-28 21:34:17 +0800

    Merge branch 'Daniel_20240428'

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		//Test Branch Daniel 20240421
+		//Test New Branch Daniel 20240428
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
