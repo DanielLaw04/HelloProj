ef1a224273a46682f3b3ffebcd2dd2811a8d5004
daniellaw <daniellaw04@yahoo.com.hk>
2024-12-19 22:28:11 +0800

    Add feature 1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,6 +13,7 @@ public class DemoApplication {
 		//Test New Branch Daniel 20240512
 
         //feature_20240512
+		//Add feature 20241219
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
