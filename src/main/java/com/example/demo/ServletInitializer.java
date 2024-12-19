7d55d01699d4aeccf3edf148ca797deeb5efb1c0
daniellaw <daniellaw04@yahoo.com.hk>
2024-12-19 22:29:09 +0800

    Add feature 2

--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -8,6 +8,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		//Test Branch Daniel 20240421
+		//Add feature 2
 		return application.sources(DemoApplication.class);
 	}
 
